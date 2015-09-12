96d20e21e1c30142a22f1e1f3d35f25f35a4691b
Prasanna <prasanmouli@gmail.com>
2015-09-12 15:10:05 -0400

    integrating walmart api

--- app.js
+++ app.js
@@ -22,10 +22,34 @@ app.use(express.static(__dirname+'/public', { maxAge: 31557600000 }));
 app.get('/', function (req, res) {
   res.render('app', { /* template locals context */ });
 });
+app.get('/user/:id', function(req, res) {
+    res.send('user ' + req.params.id);
+});
+
 
 app.use(errorHandler());
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
+
+
+var url = 'api.walmartlabs.com';
+var options = {
+  host: url,
+  port: 80,
+  path: '/v1/search?apiKey=cwbz2bqg3gd5r466mujxb38h&query=ipod',
+  method: 'GET'
+};
+
+/*http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+}).end();*/
+
+
 module.exports = app;
