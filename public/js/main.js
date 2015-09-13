d16a2aa3716b371e477336072f7dbc61b7179ad3
Nicolai Ferraris <Nicolai@0587365703.wireless.umich.net>
2015-09-12 21:20:23 -0400

    fixed + button

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,47 @@
 var leftoverBudget;
 var counter = 1;
-$(document).ready(function(){
 	var cur = 2;
-	$("#addButton").click(function(){
-		
-		var newTextBoxDiv =  $(document.createElement('div')).attr("id", 'TextButtonDiv' + counter);
 
-		newTextBoxDiv.after().html('<label>Item '+ cur + ' : </label>' +
-		      '<input type="text" name="textbox' + cur + 
-		      '" class = "boxes" id="textbox' + cur + '" value="" >');
-	            
-		newTextBoxDiv.appendTo("#TextBoxesGroup");
+$(document).ready(function(){
+	// console.log("ayo");
 
-					
-		 ++counter;
-	     ++cur;
-	     });
 
-$('#enter').click(function(){
-	// console.log(counter);
-	var currAdd = 1;
-	// console.log(currAdd);
+// $('#enter').click(function(){
+// 	// console.log(counter);
+// 	var currAdd = 1;
+// 	// console.log(currAdd);
 
-	var budget = parseInt($("#budget").val());
-	while(currAdd <= counter){
-		// console.log("here");
-		var currID = String("#textbox"+currAdd);
-		if(parseInt($(currID).val()) != NaN){
-			console.log(budget - parseInt($(currID).val()));
-		}
-		++currAdd;
-	}
-});
+// 	var budget = parseInt($("#budget").val());
+// 	while(currAdd <= counter){
+// 		// console.log("here");
+// 		var currID = String("#textbox"+currAdd);
+// 		if(parseInt($(currID).val()) != NaN){
+// 			console.log(budget - parseInt($(currID).val()));
+// 		}
+// 		++currAdd;
+// 	}
+// });
 
 	
 
 
 });
 
+	$("#addButton").click(function(e){
+		// e.stopPropagation();
+
+		    console.log("hit");
+
+		var newTextBoxDiv =  $(document.createElement('div')).attr("id", 'TextButtonDiv' + counter);
+
+		newTextBoxDiv.after().html('<label>Item '+ cur + ' : </label>' +
+		      '<input type="text" name="textbox' + cur + 
+		      '" class = "boxes" id="textbox' + cur + '" value="" >');
+	            
+
+					
+		 ++counter;
+	     ++cur;
+
+		newTextBoxDiv.appendTo("#TextBoxesGroup");
+	     });
